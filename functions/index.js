cdb2057473ab60a536bab3c03c091cb44ac22f30
unknown <mohamedmagdy12@outlook.com>
2022-08-08 18:45:28 +0200

    functions init

---
+++ functions/index.js
@@ -0,0 +1,83 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const spawn = require('child-process-promise').spawn;
+
+
+// initialise firebase admin
+admin.initializeApp();
+
+exports.createVideoThumb = functions.storage // .runWith({timeoutSeconds:300,memory:'512MB'})
+  .object()
+  .onFinalize(async object => {
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
+    const dir = path.dirname(filePath);
+    const fileName = path.basename(filePath);
+
+    functions.logger.log(fileBucket, filePath, contentType, dir, fileName);
+
+    // if this is not an image stop execution
+    if (!contentType.startsWith('video/')) return console.log('Not an video');
+
+    // if this is not the intended directory stop execution
+    // if (!dir === 'images/books')
+    //   return console.log('Not the intended directory');
+
+    if (fileName.startsWith('thumb_')) {
+      return console.log('Video is already a thumbnail');
+    }
+
+    // continue execution
+
+    // download file to memory
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+      contentType: 'image/png',
+    };
+
+    // download the file from bucket to tmp disk
+    try {
+      const upRes = await bucket
+        .file(filePath)
+        .download({destination: tempFilePath});
+      console.log('UP_RES', JSON.stringify(upRes));
+      functions.logger.log('video has been downloaded to :', tempFilePath);
+    } catch (error) {
+      console.log('err at download bucket', error);
+    }
+
+    const thmbFileName = `thumb_${fileName}.png`;
+    const locatThmbFilePath = path.join(os.tmpdir(), thmbFileName);
+    const remoteThmbFilePath = path.join(dir, thmbFileName);
+
+    await spawn('ffmpeg', [
+      '-i',
+      tempFilePath,
+      '-vframes',
+      '1',
+      '-an',
+      '-s',
+      '400X400',
+      '-ss',
+      '1',
+      locatThmbFilePath,
+    ]);
+    functions.logger.log('Thumbnail has been created');
+
+    await bucket.upload(locatThmbFilePath, {
+      destination: remoteThmbFilePath,
+      metadata: metadata,
+      public: true,
+    });
+
+    functions.logger.log('Thumbmain uploaded to the bucket');
+
+    // return 'Task done';
+    await fs.unlinkSync(locatThmbFilePath);
+    return fs.unlinkSync(tempFilePath);
+  });
